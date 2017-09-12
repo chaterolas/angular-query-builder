cb91c0b539093c6ea184029030df65317893022d
Alaín Pérez <alain.perez@nielsen.com>
2017-09-11 19:32:55 -0500

    Changing base classes to interfaces.

--- src/app/condition-builder/condition-comparison.ts
+++ src/app/condition-builder/condition-comparison.ts
@@ -1,6 +1,6 @@
 import { ConditionComparisonOperator } from './condition-comparison-operator.enum';
 
-export interface ConditionComparison  {
+export class ConditionComparison  {
     leftOperand: number | string | Array<number | string>;
     rightOperand: number | string | Array<number | string>;
     operator: ConditionComparisonOperator;
--- src/app/condition-builder/condition-group.ts
+++ src/app/condition-builder/condition-group.ts
@@ -1,7 +1,11 @@
 import { ConditionComparison } from './condition-comparison';
 import { ConditionGroupOperator } from './condition-group-operator.enum';
 
-export interface ConditionGroup {
+export class ConditionGroup {
     operator: ConditionGroupOperator;
     operands: Array<ConditionGroup | ConditionComparison>;
+
+    constructor() {
+        this.operands = new Array<ConditionGroup | ConditionComparison>();
+    }
 }
